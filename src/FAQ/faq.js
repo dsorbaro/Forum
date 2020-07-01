1cfd56b023652a1b9ee5a95a5ac8f6f1cb11af87
xJv0 <jjvo@sas.upenn.edu>
2020-07-01 16:33:35 -0400

    first draft of FAQ

--- src/FAQ/faq.js
+++ src/FAQ/faq.js
@@ -1,22 +1,29 @@
-
 import React, { Component } from "react";
+import "./faq.css";
 
 class Faq extends Component {
   constructor(props) {
     super(props);
-        this.state = {};
-    }
+    this.state = {};
+  }
 
-
-   render() {
-     return (
-       <div>
-          <p>FAQ</p>
+  render() {
+    return (
+      <div>
+        <p className="faqHeader">FAQs</p>
+        <p className="faqq1">
+          What is the difference between Public Figures and General Users?
+        </p>
+        <p className="faqa1">
+          Just as how many social media pages recognize users as "verified",
+          Forum regards those with leadership roles in their communities as
+          Public Figures. Only Public Figures can participate in a debate, but
+          both General Users and Public Figures have the capacity to vote for
+          who they would like to debate and what should be debated.
+        </p>
       </div>
-     );
-   }
- }
-
-
+    );
+  }
+}
 
 export default Faq;
